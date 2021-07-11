6575fca6ee5e6c3287be9b824bee00ec5f9bf853
Sridhar Pedamallu <sridharpedamallu@gmail.com>
2021-07-11 10:30:20 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
 const port = 3000
 
@@ -6,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World! Changed')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT ||port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
